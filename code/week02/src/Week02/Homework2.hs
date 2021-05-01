1abf9101f447da94dd4ff0bef8056bac25cc0a7c
vadimbakaev <bakaev.vad@gmail.com>
2021-05-01 19:05:00 +0200

    Homework week02

--- code/week02/src/Week02/Homework2.hs
+++ code/week02/src/Week02/Homework2.hs
@@ -1,89 +1,96 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE DeriveAnyClass      #-}
-{-# LANGUAGE DeriveGeneric       #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Week02.Homework2 where
 
-import           Control.Monad        hiding (fmap)
-import           Data.Aeson           (FromJSON, ToJSON)
-import           Data.Map             as Map
-import           Data.Text            (Text)
-import           Data.Void            (Void)
-import           GHC.Generics         (Generic)
-import           Plutus.Contract      hiding (when)
-import           PlutusTx             (Data (..))
-import qualified PlutusTx
-import           PlutusTx.Prelude     hiding (Semigroup(..), unless)
-import           Ledger               hiding (singleton)
-import           Ledger.Constraints   as Constraints
-import qualified Ledger.Scripts       as Scripts
+import Control.Monad hiding (fmap)
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Map as Map
+import Data.Text (Text)
+import Data.Void (Void)
+import GHC.Generics (Generic)
+import Ledger hiding (singleton)
+import Ledger.Ada as Ada
+import Ledger.Constraints as Constraints
+import qualified Ledger.Scripts as Scripts
 import qualified Ledger.Typed.Scripts as Scripts
-import           Ledger.Ada           as Ada
-import           Playground.Contract  (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
-import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types     (KnownCurrency (..))
-import           Prelude              (Semigroup (..))
-import           Text.Printf          (printf)
+import Playground.Contract (ToSchema)
+import Playground.TH (mkKnownCurrencies, mkSchemaDefinitions)
+import Playground.Types (KnownCurrency (..))
+import Plutus.Contract hiding (when)
+import qualified PlutusTx
+import PlutusTx.Prelude hiding (Semigroup (..), unless)
+import Text.Printf (printf)
+import Prelude (Semigroup (..))
 
 data MyRedeemer = MyRedeemer
-    { flag1 :: Bool
-    , flag2 :: Bool
-    } deriving (Generic, FromJSON, ToJSON, ToSchema)
+  { flag1 :: Bool,
+    flag2 :: Bool
+  }
+  deriving (Generic, FromJSON, ToJSON, ToSchema)
 
 PlutusTx.unstableMakeIsData ''MyRedeemer
 
-{-# INLINABLE mkValidator #-}
--- This should validate if and only if the two Booleans in the redeemer are equal!
+{-# INLINEABLE mkValidator #-}
 mkValidator :: () -> MyRedeemer -> ValidatorCtx -> Bool
-mkValidator _ _ _ = True -- FIX ME!
+mkValidator _ (MyRedeemer f1 f2) _ = f1 == f2
 
 data Typed
+
 instance Scripts.ScriptType Typed where
-    -- Implement me!
+  type DatumType Typed = ()
+  type RedeemerType Typed = MyRedeemer
 
 inst :: Scripts.ScriptInstance Typed
-inst = undefined -- FIX ME!
+inst =
+  Scripts.validator @Typed
+    $$(PlutusTx.compile [||mkValidator||])
+    $$(PlutusTx.compile [||wrap||])
+  where
+    wrap = Scripts.wrapValidator @() @MyRedeemer
 
 validator :: Validator
-validator = undefined -- FIX ME!
+validator = Scripts.validatorScript inst
 
 valHash :: Ledger.ValidatorHash
-valHash = undefined -- FIX ME!
+valHash = Scripts.validatorHash validator
 
 scrAddress :: Ledger.Address
-scrAddress = undefined -- FIX ME!
+scrAddress = ScriptAddress valHash
 
 type GiftSchema =
-    BlockchainActions
-        .\/ Endpoint "give" Integer
-        .\/ Endpoint "grab" MyRedeemer
+  BlockchainActions
+    .\/ Endpoint "give" Integer
+    .\/ Endpoint "grab" MyRedeemer
 
 give :: (HasBlockchainActions s, AsContractError e) => Integer -> Contract w s e ()
 give amount = do
-    let tx = mustPayToTheScript () $ Ada.lovelaceValueOf amount
-    ledgerTx <- submitTxConstraints inst tx
-    void $ awaitTxConfirmed $ txId ledgerTx
-    logInfo @String $ printf "made a gift of %d lovelace" amount
+  let tx = mustPayToTheScript () $ Ada.lovelaceValueOf amount
+  ledgerTx <- submitTxConstraints inst tx
+  void $ awaitTxConfirmed $ txId ledgerTx
+  logInfo @String $ printf "made a gift of %d lovelace" amount
 
 grab :: forall w s e. (HasBlockchainActions s, AsContractError e) => MyRedeemer -> Contract w s e ()
 grab r = do
-    utxos <- utxoAt scrAddress
-    let orefs   = fst <$> Map.toList utxos
-        lookups = Constraints.unspentOutputs utxos      <>
-                  Constraints.otherScript validator
-        tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toData r | oref <- orefs]
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
-    void $ awaitTxConfirmed $ txId ledgerTx
-    logInfo @String $ "collected gifts"
+  utxos <- utxoAt scrAddress
+  let orefs = fst <$> Map.toList utxos
+      lookups =
+        Constraints.unspentOutputs utxos
+          <> Constraints.otherScript validator
+      tx :: TxConstraints Void Void
+      tx = mconcat [mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toData r | oref <- orefs]
+  ledgerTx <- submitTxConstraintsWith @Void lookups tx
+  void $ awaitTxConfirmed $ txId ledgerTx
+  logInfo @String $ "collected gifts"
 
 endpoints :: Contract () GiftSchema Text ()
 endpoints = (give' `select` grab') >> endpoints
