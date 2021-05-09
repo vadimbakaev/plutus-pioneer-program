64c56577a72acff798302c8b31c5f98d01f5b263
vadimbakaev <bakaev.vad@gmail.com>
2021-05-09 12:59:15 +0200

    Homework week04

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -1,42 +1,54 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Week04.Homework where
 
-import Data.Aeson                 (FromJSON, ToJSON)
-import Data.Functor               (void)
-import Data.Text                  (Text, unpack)
-import GHC.Generics               (Generic)
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Functor (void)
+import Data.Text (Text, unpack)
+import GHC.Generics (Generic)
 import Ledger
-import Ledger.Ada                 as Ada
-import Ledger.Constraints         as Constraints
-import Plutus.Contract            as Contract
-import Plutus.Trace.Emulator      as Emulator
+import Ledger.Ada as Ada
+import Ledger.Constraints as Constraints
+import Plutus.Contract as Contract
+import Plutus.Trace.Emulator as Emulator
 import Wallet.Emulator.Wallet
 
 data PayParams = PayParams
-    { ppRecipient :: PubKeyHash
-    , ppLovelace  :: Integer
-    } deriving (Show, Generic, FromJSON, ToJSON)
+  { ppRecipient :: PubKeyHash,
+    ppLovelace :: Integer
+  }
+  deriving (Show, Generic, FromJSON, ToJSON)
 
 type PaySchema = BlockchainActions .\/ Endpoint "pay" PayParams
 
 payContract :: Contract () PaySchema Text ()
 payContract = do
-    pp <- endpoint @"pay"
-    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
-    payContract
+  pp <- endpoint @"pay"
+  let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+  handleError (logError . unpack) $ void $ submitTx tx
+  payContract
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace x y = undefined -- IMPLEMENT ME!
+payTrace x y = do
+  h1 <- activateContractWallet (Wallet 1) payContract
+  void $ activateContractWallet (Wallet 2) payContract
+  let wallet2PubKey = pubKeyHash $ walletPubKey (Wallet 2)
+  callEndpoint @"pay" h1 $
+    PayParams
+      wallet2PubKey
+      x
+  void $ Emulator.waitNSlots 1
+  callEndpoint @"pay" h1 $
+    PayParams wallet2PubKey y
+  void $ Emulator.waitNSlots 1
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
